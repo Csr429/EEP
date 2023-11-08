2c9098098332c3e2d8d8e18dee3ae781ff3eda4f
Csr429 <138554277+Csr429@users.noreply.github.com>
2023-11-07 21:30:20 -0300

    Update ex004.c

--- 2 sem/Logica de programação/2023 08 29 - Ponteiro/ex004.c
+++ 2 sem/Logica de programação/2023 08 29 - Ponteiro/ex004.c
@@ -2,27 +2,34 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void function();
+void jorge();
+int n1, n2, *ptrN1, *ptrN2;
 
-int main(){
+int main()
+{
     setlocale(LC_ALL, "");
     system("cls");
-
-    int n1, n2, *ptrN1, ptrN2;
 
     printf("Por favor, insira um novo número: ");
     scanf("%d", &n1);
     printf("Por favor, insira um novo número: ");
     scanf("%d", &n2);
 
-    function(n1, n2);
+        printf("\n Conteúdo atual de N1: %d. Endereço atual de N1: %p. Conteúdo Atual de N2: %d. Endereço atual de N2: %p", n1, &n1, n2, &n2);
 
-    
-    
+    jorge(n1, n2);
+
+    printf("\n Conteúdo atual de N1: %d. Endereço atual de N1: %p. Conteúdo Atual de N2: %d. Endereço atual de N2: %p", n1, &n1, n2, &n2);
 }
 
-function(n1, n2){
-    ptrN1 = &n1;
-    ptrN2 = &n2;
+void jorge()
+{
+    int registrador; // Variável que irá¡ armazenar temporariamente o valor de uma variável para que seja possível a troca
 
+    ptrN1=&n1;
+    ptrN2=&n2;
+
+    registrador = *ptrN1;
+    *ptrN1 = *ptrN2;
+    *ptrN2 = registrador; 
 };
