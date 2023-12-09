5f513a4b1adaa9df56dd5cbbcc5b6658971e7cfa
Csr429 <138554277+Csr429@users.noreply.github.com>
2023-12-09 07:59:24 -0300

    Create analiseDeResposta.c

---
+++ 2 sem/Logica de programação/revisão/analiseDeResposta.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int inteiro();
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    system("cls");
+}
+
+int inteiro()
+{
+    for(int x = 0; )
+}
